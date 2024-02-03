ee1266a776b41f249de988b4b381c74f4e1538f5
Shen <863461783@qq.com>
2024-02-03 10:29:59 +0800

    Initial Commit

---
+++ ShimmerEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShimmerEffect
+//
+//  Created by 香饽饽zizizi on 2024/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by 香饽饽zizizi on 2024/2/3.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
